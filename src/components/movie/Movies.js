f755157f8480aeda5323a298d52fc6152d420283
Arya Augusta <aryaaugusta@gmail.com>
2024-02-06 23:28:53 +0700

    add logic

--- src/components/movie/Movies.js
+++ src/components/movie/Movies.js
@@ -1,4 +1,5 @@
 import {Card, Col, Container, Image, Row} from "react-bootstrap"
+import React from "react";
 
 const Movies = ({movies}) => {
     const baseImg = process.env.REACT_APP_IMG_URL
@@ -11,36 +12,40 @@ const Movies = ({movies}) => {
                 <br/>
                 <Row>
                     {movies.map((result, index) => {
-                        return (
-                            <Col md={3} className="movieWrapper" id="movies" key={index}>
-                                <Card className="movieImage">
-                                    <div className="movieTitle">
-                                        <Card.Title>{result.title}</Card.Title>
-                                    </div>
-                                    <Image src={`${baseImg}/${result.posterPath}`}
-                                           className="images"/>
-                                    <div className="bg-dark">
-                                        <div className="p-2 m-1 text-white">
-                                            <div className="movieOverview">
-                                                <Card.Text className="text-left">
-                                                    {result.overview}
-                                                </Card.Text>
-                                            </div>
-                                            <div className="movieDate">
-                                                <Card.Text className="text-left">
-                                                    release: {result.releaseDate}
-                                                </Card.Text>
-                                            </div>
-                                            <div className="movieRate">
-                                                <Card.Text className="text-left">
-                                                    rate: {result.voteAverage}
-                                                </Card.Text>
+                        for (let x of result.movieDetails) {
+                            return (
+                                <Col md={3} className="movieWrapper" id="movies" key={index}>
+                                    <Card className="movieImage">
+                                        <div className="movieTitle">
+                                            <Card.Title>{result.title}</Card.Title>
+                                        </div>
+                                        <Image src={`${baseImg}/${x.posterPath}`}
+                                               className="images"/>
+                                        <div className="bg-dark">
+                                            <div className="p-2 m-1 text-white">
+                                                <div className="movieOverview">
+                                                    <Card.Text className="text-left">
+                                                        {x.overview}
+                                                    </Card.Text>
+                                                </div>
+                                                <div className="movieDate">
+                                                    <Card.Text className="text-left">
+                                                        release: {x.releaseDate}
+                                                        <Card.Text className="movieRate">
+                                                            rate: {x.voteAverage}
+                                                        </Card.Text>
+                                                    </Card.Text>
+
+                                                </div>
+                                                <div className="movieRate">
+
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </Card>
-                            </Col>
-                        )
+                                    </Card>
+                                </Col>
+                            )
+                        }
                     })}
                 </Row>
             </Container>
