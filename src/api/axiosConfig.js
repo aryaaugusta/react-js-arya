131feae9413343fbb025d359f379008e1b4cfba3
Arya Augusta <aryaaugusta@gmail.com>
2024-02-06 23:26:51 +0700

    update axios

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',//'https://9c96-103-106-239-104.ap.ngrok.io'
+    baseURL: 'http://localhost:8080/',//'https://9c96-103-106-239-104.ap.ngrok.io'
     headers: {"ngrok-skip-browser-warning": "true"}
 })
