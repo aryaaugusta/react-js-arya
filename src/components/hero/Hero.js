1a63066d48939f2842a0a7921ece3aa47c0c62ff
Arya Augusta <aryaaugusta@gmail.com>
2024-02-06 23:27:36 +0700

    update some item

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,51 +3,47 @@ import Carousel from 'react-material-ui-carousel';
 import {Paper} from '@mui/material';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCirclePlay} from '@fortawesome/free-solid-svg-icons';
-import {Link, useNavigate} from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import {Link} from "react-router-dom";
 
 const Hero = ({movies}) => {
-
-    /*const navigate = useNavigate();
-
-    function reviews(movieId) {
-        navigate(`/Reviews/${movieId}`);
-    }*/
     const baseImg = process.env.REACT_APP_IMG_URL
     return (
         <div className='movie-carousel-container'>
             <Carousel>
                 {
                     movies?.map((movie) => {
-                        return (
-                            <Paper key={movie.id}>
-                                <div className='movie-card-container'>
-                                    <div className="movie-card" style={{"--img": `url(${baseImg}/${movie.backdropPath})`}}>
-                                        <div className="movie-detail">
-                                            <div className="movie-poster">
-                                                <img src={`${baseImg}/${movie.posterPath}`} alt=""/>
-                                            </div>
-                                            <div className="movie-title">
-                                                <h3>{movie.title}</h3>
-                                                <div className="movie-buttons-container">
-                                                    <Link
-                                                        to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
-                                                        <div className="play-button-icon-container">
-                                                            <FontAwesomeIcon className="play-button-icon"
-                                                                             icon={faCirclePlay}/>
-                                                        </div>
-                                                    </Link>
-                                                    {/*<div className="movie-review-button-container">
+                        for (let x of movie.movieDetails) {
+                            return (
+                                <Paper key={movie.id}>
+                                    <div className='movie-card-container'>
+                                        <div className="movie-card"
+                                             style={{"--img": `url(${baseImg}/${x.backdropPath})`}}>
+                                            <div className="movie-detail">
+                                                <div className="movie-poster">
+                                                    <img src={`${baseImg}/${x.posterPath}`} alt=""/>
+                                                </div>
+                                                <div className="movie-title">
+                                                    <h3>{movie.title}</h3>
+                                                    <div className="movie-buttons-container">
+                                                        <Link
+                                                            to={`/Trailer/${x.trailerLink.substring(x.trailerLink.length - 11)}`}>
+                                                            <div className="play-button-icon-container">
+                                                                <FontAwesomeIcon className="play-button-icon"
+                                                                                 icon={faCirclePlay}/>
+                                                            </div>
+                                                        </Link>
+                                                        {/*<div className="movie-review-button-container">
                                                     <Button variant="info"
                                                             onClick={() => reviews(movie.imdbId)}>Reviews</Button>
                                                 </div>*/}
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </Paper>
-                        )
+                                </Paper>
+                            )
+                        }
                     })
                 }
             </Carousel>
